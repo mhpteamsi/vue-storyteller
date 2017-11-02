732ca8ed2efaddb6b67cacc884c58db191c8fb4d
jmotes <jonathanmotes@gmail.com>
2017-11-02 00:17:25 -0500

    Cleanup

--- src/components/Teaser.vue
+++ src/components/Teaser.vue
@@ -12,9 +12,9 @@
         define in the schema in the blok variable
         -->
         {{blok.headline}}
-        </h1>
-        <h2>
-          You can create new components like this - to create your own set of components.
+      </h1>
+      <h2>
+        You can create new components like this - to create your own set of components.
       </h2>
     </div>
   </div>
