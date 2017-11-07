88e65148e2b38e14e45471afd7219fada9452cb3
jmotes <jonathanmotes@gmail.com>
2017-11-06 20:42:17 -0600

    Renaming HelloWorld component to Index

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Index from '@/components/Index'
 
 Vue.use(Router)
 
@@ -13,7 +13,7 @@ export default new Router({
     {
       path: '/:slug',
       name: 'Hello',
-      component: HelloWorld
+      component: Index
     }
   ]
 })
