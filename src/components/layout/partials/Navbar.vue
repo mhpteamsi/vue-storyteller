d18f9b5efec68e75b3459cf3c6c700497f726738
jmotes <jonathanmotes@gmail.com>
2017-11-06 22:55:46 -0600

    Better logo link

--- src/components/layout/partials/Navbar.vue
+++ src/components/layout/partials/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
-      <a class="navbar-brand" href="/">
+      <router-link to="/" class="navbar-brand">
         <img src="../../../assets/logo.png" width="50" height="50" alt="Add your brand name here">
-      </a>
+      </router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
