fe0637946c7330c2facfad93200339da7673a346
jmotes <jonathanmotes@gmail.com>
2017-11-09 21:11:15 -0600

    Adding favicon. Closes #10. Also a bit of cleanup.

--- src/components/CallToActionItem.vue
+++ src/components/CallToActionItem.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-editable="blok" class="card mb-5 border-0">
-    <img class="card-img-top" v-lazy="resizeImage(blok.image, '350x233')" alt="Card image cap">
+    <img class="card-img-top" v-lazy="optimizedImage" alt="Card image cap">
     <div class="card-body px-0">
       <h4 class="card-title">{{blok.title}}</h4>
       <p class="card-text">{{blok.text}}</p>
     </div>
     <div class="card-action">
-      <router-link :to="blok.action_link" class="btn btn-block btn-outline-primary text-uppercase">{{blok.action_label}}</router-link>
+      <router-link :to="blok.action_link" class="btn btn-block btn-outline-primary text-uppercase">
+        {{blok.action_label}}
+      </router-link>
     </div>
   </div>
 </template>
@@ -15,6 +17,11 @@
   import utility from '@/mixins/utility'
   export default {
     props: ['blok'],
+    computed: {
+      optimizedImage () {
+        return this.resizeImage(this.blok.image, '350x233')
+      }
+    },
     mixins: [ utility ]
   }
 </script>
