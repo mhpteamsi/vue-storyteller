b8445a232f221610d875f7a325f485eb24f04ca4
jmotes <jonathanmotes@gmail.com>
2017-11-10 00:06:28 -0600

    Initial work on dynamic nav for #2

--- src/components/layout/partials/Navbar.vue
+++ src/components/layout/partials/Navbar.vue
@@ -8,12 +8,9 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <router-link to="/home" class="nav-link">Home <span class="sr-only">(current)</span></router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/about" class="nav-link">About</router-link>
+        <ul class="navbar-nav mr-auto bg-red">
+          <li class="nav-item" v-for="link in links" :key="link.id">
+            <router-link :to="link.slug" class="nav-link">{{link.name}}</router-link>
           </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
@@ -24,3 +21,28 @@
     </div><!-- /.container -->
   </nav>
 </template>
+
+<script>
+// may need to change this to reference a root instance of axios
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      name: '',
+      links: []
+    }
+  },
+  mounted () {
+    // this will probably be a huge request once there's alot of pages, but it works for now
+    var reqUrl = 'https://api.storyblok.com/v1/cdn/links?token=KeYfBxUXGP0hQ7rVuWQuSwtt'
+
+    axios.get(reqUrl).then(response => {
+      var responseLinks = response.data.links
+
+      this.links = Object.keys(responseLinks).reduce((r, k) => {
+        return r.concat(responseLinks[k])
+      }, [])
+    })
+  }
+}
+</script>
