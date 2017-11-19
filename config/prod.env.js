00fb64e710da4589fc4b1f30e8b35cc417af7d4f
jmotes <jonathanmotes@gmail.com>
2017-11-19 00:55:49 -0600

    Adding API token env variable to test and prod environments.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production')
+  NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production'),
+  API_TOKEN: JSON.stringify(process.env.API_TOKEN || 'b1Ucl4d8aQHRFtab6aczTgtt')
 }
