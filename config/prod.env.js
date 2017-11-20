20c22232920b8b5b583cd05a1c58532c287fb408
jmotes <jonathanmotes@gmail.com>
2017-11-19 23:46:06 -0600

    Renaming  DEPLOY_URL to DEPLOY_PRIME_URL

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  DEPLOY_URL: JSON.stringify(process.env.DEPLOY_PRIME_URL || 'http://localhost'),
+  DEPLOY_PRIME_URL: JSON.stringify(process.env.DEPLOY_PRIME_URL || 'http://localhost'),
   API_TOKEN: JSON.stringify(process.env.API_TOKEN || 'b1Ucl4d8aQHRFtab6aczTgtt')
 }
